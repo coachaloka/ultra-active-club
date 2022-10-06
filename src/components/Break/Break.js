f4c3b3307290b8c795ac3539a72336f23328d685
coachaloka <coachaloka@gmail.com>
2022-10-06 15:42:29 +0600

    add break

--- src/components/Break/Break.js
+++ src/components/Break/Break.js
@@ -3,23 +3,23 @@ import './Break.css';
 
 
 const Break = () => {
-    const [ breaktime, setBreakTime ] = useState(0);
-
+    const [breakTime, setBreakTime] = useState(0);
+    
     return (
         <div>
-           <div>
+            <div>
             <h3>Add a Break</h3>
-           </div>
-           <div className='break'>
-                <button onClick={()=> setBreakTime(5)} className="break-times">5m</button>
-                <button onClick={()=> setBreakTime(6)} className="break-times">6m</button>
-                <button onClick={()=> setBreakTime(7)} className="break-times">7m</button>
-                <button onClick={()=> setBreakTime(8)} className="break-times">8m</button>
+            <div className="break">
+                <button onClick={()=>setBreakTime(5)} className="break-times">5m</button>
+                <button onClick={()=>setBreakTime(7)} className="break-times">7m</button>
+                <button onClick={()=>setBreakTime(8)} className="break-times">8m</button>
+                <button onClick={()=>setBreakTime(9)} className="break-times">9m</button>
+                <button onClick={()=>setBreakTime(10)} className="break-times">10m</button>
             </div>
             <div>
-                <h4 className='break-times'>Break Time: (breaktime)m</h4>
+                <h4 className="break-times">Break Time: {breakTime} m </h4>
             </div>
-            
+        </div>
         </div>
     );
 };
